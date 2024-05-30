7b70785b0474d4db327044b3698655e79fd1ee8a
Do-hyun Ko <dohyun682@gmail.com>
2024-05-30 10:52:47 +0900

    :bug: Fix env load position

--- src/database.py
+++ src/database.py
@@ -1,9 +1,10 @@
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-
+load_dotenv(override=True)
 
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
