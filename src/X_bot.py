03834f4e71e30cd9a32f2d068e8cfd142134e18b
inthree3 <42310616+inthree3@users.noreply.github.com>
2024-06-02 12:43:48 +0900

    fix: delete unnecessary code

--- src/X_bot.py
+++ src/X_bot.py
@@ -11,11 +11,6 @@ X_API_KEY_SECRET=os.getenv("X_API_KEY_SECRET")
 ACCESS_TOKEN=os.getenv("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET=os.getenv("ACCESS_TOKEN_SECRET")
 # %%
-print(X_API_KEY)
-print(X_API_KEY_SECRET)
-print(ACCESS_TOKEN)
-print(ACCESS_TOKEN_SECRET)
-# %%
 client = tweepy.Client(
     consumer_key=X_API_KEY,
     consumer_secret=X_API_KEY_SECRET,
@@ -25,5 +20,3 @@ client = tweepy.Client(
 # %%
 def create_tweet(text):
     return client.create_tweet(text)
-# %%
-client.create_tweet(text="TEST TWEET")
